b1a10326593ca7432af2a3aae0332fd08bd9bb50
Jake Kevin Klair L. Cuenca <jlcuenca.student@asiancollege.edu.ph>
2026-08-30 17:08:28 +0800

    Load redesigned project showcase

--- assets/js/site.js
+++ assets/js/site.js
@@ -17,6 +17,7 @@
         loadStylesheet("layout-v6.css?v=20260830-1");
         loadStylesheet("layout-v7.css?v=20260830-5");
         loadStylesheet("../assets/css/experience.css?v=20260830-1");
+        loadStylesheet("layout-v8.css?v=20260830-1");
     } else if (document.body?.classList.contains("photography-page")) {
         loadStylesheet("layout-v2.css?v=20260830-2");
         loadStylesheet("layout-v3.css?v=20260830-1");
@@ -146,7 +147,7 @@
         }
 
         if (document.body.classList.contains("dev-page")) {
-            tasks.push(loadScript("../assets/js/project-scene.js?v=20260830-2"));
+            tasks.push(loadScript("../assets/js/project-scene.js?v=20260830-3"));
             tasks.push(loadScript("../assets/js/leadership-archive.js?v=20260830-5"));
         }
 
